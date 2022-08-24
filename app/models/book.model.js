72ff5452e36bdbef7e07cf13a76fb8388328b5e1
Aviv Elbag <galois@Avivs-MacBook-Pro.local>
2022-08-24 14:26:57 +0200

    updated the database to include chapters

--- app/models/book.model.js
+++ app/models/book.model.js
@@ -1,13 +1,29 @@
 module.exports = (sequelize, Sequelize) => {
     const Book = sequelize.define("book", {
+      bookUUID: {
+        type:Sequelize.INTEGER,
+        autoIncrement:true,
+        allowNull:false,
+        primaryKey:true
+      },      
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull:false
       },
       dateUploaded: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull:false
       },
       published: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        allowNull:false
+      },
+      FileType:{
+        type:Sequelize.STRING,
+        allowNull:false
+      },
+      NumChapters:{
+        type:Sequelize.INTEGER
       }
     });
 
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const path = __dirname + '/app/views/dist/';
 const cors = require("cors");
 const app = express();
 const http = require("http").createServer(app);
+const faker = require("faker");
+
+
+DEVELOPMENT_MODE = true;
+
+
 const io = require("socket.io")(http, {
     cors:{
         origin:"http://localhost:5173"
@@ -46,24 +52,62 @@ app.use(express.urlencoded({ extended: true }));
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
+
+
 // controllers
 const bookController = require("./app/controllers/book.controller");
-const videoController = require("./app/controllers/video.controller");
-const articleController = require("./app/controllers/article.controller");
-const userController = require("./app/controllers/user.controller");
+// const videoController = require("./app/controllers/video.controller");
+// const articleController = require("./app/controllers/article.controller");
+// const userController = require("./app/controllers/user.controller");
 const roleController = require("./app/controllers/role.controller");
+const chapterController = require("./app/controllers/chapter.controller");
 
+
+// DEVELOPMENT MODE
+
+//For generating fake data
+var fake_books = [...Array(10)].map((user) => (
+  {
+    title:faker.commerce.productName(),
+    dateUploaded: new Date(),
+    published: true,
+    FileType:".epub",
+    NumChapters:faker.mersenne.rand(20,5)
+  }
+
+));
+var chapters = Array();
+book_number = 0;
+for(book of fake_books){
+  for(chapter_num of Array(book.NumChapters).keys()){
+    chapters.push({
+      bookUUID:book_number,
+      chapterName: faker.name.firstName(),
+      chapterNumber:chapter_num,
+      dateUploaded: new Date(),
+      chapterContent: faker.lorem.paragraphs(5)
+    })
+  }
+}
 const initDb = async () => {
   roleController.create({ id: 1, name: "user" })
   roleController.create({ id: 2, name: "moderator" })
   roleController.create({ id: 3, name: "admin" })
+  for(book of fake_books){
+    bookController.create(book);
+  }
+  for(chapter of chapters){
+    chapterController.create(chapter);
+  }
 };
-
 // database
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
   initDb();
 });
+
+//END OF DEVELOPMENT MODE
+
 
 // simple route
 app.get("/", (req, res) => {
