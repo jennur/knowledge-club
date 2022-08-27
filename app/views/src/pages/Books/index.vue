e4ecad9894d782ff02a674843f7626062961c50b
Aviv Elbag <galois@Avivs-MacBook-Pro.local>
2022-08-27 20:40:05 +0200

    removed notebook and added linkes from books to chapters

--- app/views/src/pages/Books/index.vue
+++ app/views/src/pages/Books/index.vue
@@ -7,7 +7,7 @@
   <div class="about">
     <h1>All books</h1>
     <div v-for="book in store.state.books.books" :key="book.title" class="mt-4">
-      {{book.title}}
+      <RouterLink :to="`/books/${book.bookUUID}/chapters`" class="text-xl text-rose-500 py-2 mr-6">{{book.title}}</RouterLink>
       </div>
   </div>
 </template>
