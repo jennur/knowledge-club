160d180f74de92f383e3de6c55eff33fc25ab1a3
Aviv Elbag <galois@Avivs-MacBook-Pro.local>
2022-08-28 17:42:42 +0200

    Highlights are detected

--- app/views/src/pages/Books/Chapters/Text/index.vue
+++ app/views/src/pages/Books/Chapters/Text/index.vue
@@ -3,6 +3,26 @@ import store from "../../../../store/index"
 import {useRoute} from "vue-router";
 const route = useRoute();
 store.dispatch("chapters/getChapter",{bookId:route.params.id,chapterNum:route.params.chapternum})
+
+function getSelectedText() {
+    var text = "";
+    if (typeof window.getSelection != "undefined") {
+        text = window.getSelection().toString();
+    } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+function doSomethingWithSelectedText() {
+    var selectedText = getSelectedText();
+    if (selectedText) {
+        alert("Got selected text " + selectedText);
+    }
+}
+
+document.onmouseup = doSomethingWithSelectedText;
+document.onkeyup = doSomethingWithSelectedText;
 </script>
 <template>
   <div class="about">
