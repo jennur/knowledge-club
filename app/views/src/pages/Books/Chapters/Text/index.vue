cb260f9809ca1c0e2bd1166ff9300d6d7374eee2
Aviv Elbag <galois@Avivs-MacBook-Pro.local>
2022-08-29 13:01:02 +0200

    visual highlighting... Buggy

--- app/views/src/pages/Books/Chapters/Text/index.vue
+++ app/views/src/pages/Books/Chapters/Text/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import store from "../../../../store/index"
 import {useRoute} from "vue-router";
+import { onMounted } from 'vue'
 const route = useRoute();
 
 store.dispatch("chapters/getChapter",{
@@ -14,6 +15,7 @@ function getSelectedText() {
     if (typeof window.getSelection != "undefined") {
         text = window.getSelection();
         text_range = {startloc:text.anchorOffset,endloc:text.focusOffset}
+
     } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
         text = document.selection.createRange();
     }
@@ -22,26 +24,37 @@ function getSelectedText() {
 
 function doSomethingWithSelectedText() {
     var selectedText = getSelectedText();
-    store.dispatch("chapters/postHighlight", {
-      bookId:route.params.id,
-      chapterNum:route.params.chapternum,
-      startloc:selectedText.startloc,
-      endloc:selectedText.endloc,
-      fromUser:store.state.auth.user.username,
-      content:""
+    if(selectedText.startloc!=selectedText.endloc){
+      store.dispatch("chapters/postHighlight", {
+        bookId:route.params.id,
+        chapterNum:route.params.chapternum,
+        startloc:selectedText.startloc,
+        endloc:selectedText.endloc,
+        fromUser:store.state.auth.user.username,
+        content:""
+      })
     }
-      );
-    
 }
-// function highlight(startloc,endloc) {
-//   var inputText = document.getElementsByClassName("chapter-content");
-//   var innerHTML = inputText.innerHTML;
-//   if (startloc >= 0) { 
-//    innerHTML = innerHTML.substring(0,startloc) + "<span class='highlight'>" + innerHTML.substring(startloc,endloc) + "</span>" + innerHTML.substring(endloc);
-//    inputText.innerHTML = innerHTML;
-//   }
-// }
-// highlight(5,10);
+  
+function do_highlight(startloc,endloc,inputText) {
+  var innerHTML = inputText.innerHTML;
+  console.log(innerHTML)
+  if (startloc >= 0) { 
+   innerHTML = innerHTML.substring(0,startloc) + "<span class='highlight'>" + innerHTML.substring(startloc,endloc) + "</span>" + innerHTML.substring(endloc);
+   inputText.innerHTML = innerHTML;
+  }
+}
+
+onMounted(()=>{
+  var inputText = document.getElementsByClassName("chapter-content")[0];
+  for(let k of store.state.chapters.focusedChapter.highlights){
+    console.log(k)
+    inputText = document.getElementsByClassName("chapter-content")[0];
+    do_highlight(k.startloc,k.endloc,inputText);
+  }
+  
+})
+
 
 document.onmouseup = doSomethingWithSelectedText;
 document.onkeyup = doSomethingWithSelectedText;
@@ -53,5 +66,8 @@ document.onkeyup = doSomethingWithSelectedText;
 </template>
 
 <style>
+.highlight{
+  background-color: aqua;
+}
 
 </style>
