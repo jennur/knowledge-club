33ea6a0b18031bfb1f500591d4733c06504a51fc
Aviv Elbag <galois@Avivs-MacBook-Pro.local>
2022-08-31 14:53:38 +0200

    fixed the highlighting issue with overwriting on the innerHTML

---
+++ app/views/src/components/Slideover/Slideover.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+function toggleSlideover(){
+    document.getElementById('slideover').classList.toggle('translate-x-full');
+}
+</script>
+
+<template>
+<div>
+    <div class="h-full w-96 bg-slate-200 absolute right-0 duration-300 ease-out transition-all translate-x-full flex-col" id="slideover" >
+        <div id="interaction-menu" class="flex flex-row">
+        <font-awesome-icon icon="fa-solid fa-xmark" class="flex-1/12 text-2xl mt-2 ml-2" @click="toggleSlideover"/>
+        <input type="text" class="flex-11/12 grow ml-10 mt-1">
+        </div>
+        <div id="tool-controller" class="flex flex-col">
+            <div>
+                <input type="checkbox" class="default:ring-4" id="checkbox1"/> 
+                <label for="checkbox1">Blah</label>
+            </div>
+            <div>
+                <input type="checkbox" class="default:ring-4" id="checkbox1"/> 
+                <label for="checkbox1">Blah</label>
+            </div>
+            <div>
+                <input type="checkbox" class="default:ring-4" id="checkbox1"/> 
+                <label for="checkbox1">Blah</label>
+            </div>
+            <div>
+                <input type="checkbox" class="default:ring-4" id="checkbox1"/> 
+                <label for="checkbox1">Blah</label>
+            </div>
+        </div>
+        
+    </div>
+</div>
+</template>
--- app/views/src/pages/Books/Chapters/Text/index.vue
+++ app/views/src/pages/Books/Chapters/Text/index.vue
@@ -2,6 +2,7 @@
 import store from "../../../../store/index"
 import {useRoute} from "vue-router";
 import { onMounted } from 'vue'
+import Slideover from "../../../../components/Slideover/Slideover.vue"
 const route = useRoute();
 
 store.dispatch("chapters/getChapter",{
@@ -12,9 +13,15 @@ store.dispatch("chapters/getChapter",{
 function getSelectedText() {
     var text = "";
     var text_range = {startloc:0,endloc:0};
-    if (typeof window.getSelection != "undefined") {
+    
+    if(!window.getSelection().focusNode.previousSibling){
+      text = window.getSelection();
+      text_range = {startloc:text.anchorOffset,endloc:text.focusOffset}
+    }
+    else if (typeof window.getSelection != "undefined") {
+        let last_loc= parseInt(window.getSelection().focusNode.previousSibling.attributes.loc.value)
         text = window.getSelection();
-        text_range = {startloc:text.anchorOffset,endloc:text.focusOffset}
+        text_range = {startloc:last_loc + text.anchorOffset,endloc:last_loc + text.focusOffset}
 
     } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
         text = document.selection.createRange();
@@ -24,6 +31,7 @@ function getSelectedText() {
 
 function doSomethingWithSelectedText() {
     var selectedText = getSelectedText();
+
     if(selectedText.startloc!=selectedText.endloc){
       store.dispatch("chapters/postHighlight", {
         bookId:route.params.id,
@@ -36,22 +44,46 @@ function doSomethingWithSelectedText() {
     }
 }
   
-function do_highlight(startloc,endloc,inputText) {
-  var innerHTML = inputText.innerHTML;
-  console.log(innerHTML)
-  if (startloc >= 0) { 
-   innerHTML = innerHTML.substring(0,startloc) + "<span class='highlight'>" + innerHTML.substring(startloc,endloc) + "</span>" + innerHTML.substring(endloc);
-   inputText.innerHTML = innerHTML;
+function do_highlight(sortedHighlights,inputText) {
+  let original_chapter_content = store.state.chapters.focusedChapter.data.chapterContent
+  let out = ""
+  let cur_index = original_chapter_content.length;
+  let cur_highlight =""
+  for(let highlight of sortedHighlights){
+    if(highlight.endloc<cur_index){
+      console.log(highlight.endloc,cur_index)
+      console.log(original_chapter_content.substring(highlight.endloc,cur_index))
+      out = original_chapter_content.substring(highlight.endloc,cur_index-1) + out
+    }
+    
+    cur_highlight = `<span class='highlight' loc=${highlight.endloc}>` + original_chapter_content.substring(highlight.startloc,highlight.endloc)  + "</span>"
+    out = cur_highlight + out
+    cur_index = highlight.startloc
+  }
+  if (cur_index>0){
+    out = original_chapter_content.substring(0,cur_index) + out
+  }
+  console.log(original_chapter_content)
+  inputText.innerHTML = out
+
+}
+function highlightSort(a,b){
+  if(a.startloc>b.startloc){
+    return -1;
+  }
+  else if(a.startloc<b.startloc){
+    return 1
+  }
+  else{
+    return 0
   }
 }
 
 onMounted(()=>{
   var inputText = document.getElementsByClassName("chapter-content")[0];
-  for(let k of store.state.chapters.focusedChapter.highlights){
-    console.log(k)
-    inputText = document.getElementsByClassName("chapter-content")[0];
-    do_highlight(k.startloc,k.endloc,inputText);
-  }
+  let allHighlights = store.state.chapters.focusedChapter.highlights.sort(highlightSort)
+
+  do_highlight(allHighlights,document.getElementsByClassName("chapter-content")[0]);
   
 })
 
@@ -60,9 +92,13 @@ document.onmouseup = doSomethingWithSelectedText;
 document.onkeyup = doSomethingWithSelectedText;
 </script>
 <template>
-  <div class="chapter-content">
+  <div class="chapter">
+    <Slideover/>
+    <div class="chapter-content">
     {{ store.state.chapters.focusedChapter.data.chapterContent }}
+    </div>
   </div>
+  
 </template>
 
 <style>
