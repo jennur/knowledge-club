dbf0650c25cbc5027a2a5b741357d486fecb2c7b
Jenny Bonsak <jenny.bonsak@gmail.com>
2022-09-03 15:56:09 +0200

    Add new logo and favicon

--- app/views/src/layouts/Header/LeftMenu.vue
+++ app/views/src/layouts/Header/LeftMenu.vue
@@ -4,8 +4,18 @@
 
 <template>
   <nav class="flex flex-1 items-center">
-    <RouterLink to="/" class="text-lg lg:text-xl text-rose-500 py-2">
-      <font-awesome-icon icon="fas fa-spa" class="mr-2" />Knowledge Club
+    <RouterLink to="/" class="flex flex-1 items-center py-2">
+      <img src="/yedia-logo.svg" class="logo mr-1"/> 
+      <span class="title text-lg lg:text-2xl text-rose-500">Yedia</span>
     </RouterLink>
   </nav>
 </template>
+
+<style lang="postcss" scoped>
+  .logo {
+    height: 1.8rem;
+  }
+  .title {
+    font-family: "Itim";
+  }
+</style>
