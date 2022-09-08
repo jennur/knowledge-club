bbc2ca4b6441e38f8263cc98938667a699e32c2b
Jenny Bonsak <jenny.bonsak@gmail.com>
2022-09-08 17:00:40 +0200

    Remove hardcoded python server url

--- app/views/src/http-py.js
+++ app/views/src/http-py.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default  axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: import.meta.env.VITE_PYTHON_SERVER_URL,
     headers: {
       "Content-type": "application/json"
     }
