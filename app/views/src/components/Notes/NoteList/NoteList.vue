1b8ff0fce6d962d7858caf64c13cec0838d0d9d5
Jenny Bonsak <jenny.bonsak@gmail.com>
2022-09-09 20:21:48 +0200

    Add sorting to highlight notes, to show them chronologically

--- app/views/src/components/Notes/NoteList/NoteList.vue
+++ app/views/src/components/Notes/NoteList/NoteList.vue
@@ -1,17 +1,19 @@
 <script setup>
   import { ref, reactive, computed, watch } from 'vue'
+  import highlightSort from "@/helpers/highlightFunctions/highlightSort.js";
   import store from "@/store/index.js";
   import Note from "./Note.vue";
 
-  const highlights = computed(() => store.state.chapters.focusedChapter.highlights);
+  const storeHighlights = computed(() => store.state.chapters.focusedChapter.highlights);
+  const highlights = computed(() => {
+    let highlightList = storeHighlights.value;
+    return highlightList.sort(highlightSort).reverse();
+  });
 </script>
 
 <template>
   <div>
-    <div 
-      v-for="highlight in store.state.chapters.focusedChapter.highlights" 
-      :key="highlight.highlightId"
-    >
+    <div v-for="highlight in highlights" :key="highlight.highlightId">
       <Note :highlightText="highlight.content" :articles="highlight.articles"/>
     </div>
   </div>
